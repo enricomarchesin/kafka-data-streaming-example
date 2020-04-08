0e6a475038c393f8b9f5000961db213646f0295d
Enrico Marchesin <enrico@cainthus.com>
2020-04-08 08:51:26 +0100

    started implementing Faust app

--- consumers/faust_stream.py
+++ consumers/faust_stream.py
@@ -1,5 +1,6 @@
 """Defines trends calculations for stations"""
 import logging
+from dataclasses import asdict
 
 import faust
 
@@ -29,29 +30,39 @@ class TransformedStation(faust.Record):
     line: str
 
 
-# TODO: Define a Faust Stream that ingests data from the Kafka Connect stations topic and
-#   places it into a new topic with only the necessary information.
 app = faust.App("stations-stream", broker="kafka://localhost:9092", store="memory://")
-# TODO: Define the input Kafka Topic. Hint: What topic did Kafka Connect output to?
-# topic = app.topic("TODO", value_type=Station)
-# TODO: Define the output Kafka Topic
-# out_topic = app.topic("TODO", partitions=1)
-# TODO: Define a Faust Table
-#table = app.Table(
+
+topic = app.topic("org.chicago.cta.connect.stations", value_type=Station)
+out_topic = app.topic("org.chicago.cta.all_stations", value_type=TransformedStation)
+
+# table = app.Table(
 #    # "TODO",
 #    # default=TODO,
 #    partitions=1,
 #    changelog_topic=out_topic,
-#)
+# )
 
 
-#
-#
-# TODO: Using Faust, transform input `Station` records into `TransformedStation` records. Note that
-# "line" is the color of the station. So if the `Station` record has the field `red` set to true,
-# then you would set the `line` of the `TransformedStation` record to the string `"red"`
-#
-#
+@app.agent(topic)
+async def station_cleaner(stations):
+    async for station in stations:
+        print(asdict(station))
+        if station.red:
+            line = 'red'
+        elif station.blue:
+            line = 'blue'
+        elif station.green:
+            line = 'green'
+        else:
+            line = ''
+
+        t_station = TransformedStation(
+            station_id=station.station_id,
+            station_name=station.station_name,
+            order=station.order,
+            line=line,
+        )
+        await out_topic.send(value=t_station)
 
 
 if __name__ == "__main__":
